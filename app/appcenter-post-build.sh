adcfdb3d3324a33b2aab12d25dc3c8b1489a49b2
Gary Cheng <d9369604@gmail.com>
2019-04-26 13:46:17 +0800

    Modify the app-path

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -11,7 +11,7 @@ ls $APPCENTER_OUTPUT_DIRECTORY
 
 /Users/vsts/agent/2.150.0/work/1/s/gradlew assembleAndroidTest
 
-cp $APPCENTER_SOURCE_DIRECTORY/app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk $APPCENTER_OUTPUT_DIRECTORY
+#cp $APPCENTER_SOURCE_DIRECTORY/app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk $APPCENTER_OUTPUT_DIRECTORY
 
 echo 'ls output folder'
 ls $APPCENTER_OUTPUT_DIRECTORY
@@ -19,13 +19,13 @@ ls $APPCENTER_OUTPUT_DIRECTORY
 appcenter test run espresso \
     --app "d9369604-gmail.com/Hello-Music" \
     --devices c9eaa83b \
-    --app-path $APPCENTER_OUTPUT_DIRECTORY/app-debug-androidTest.apk  \
+    --app-path $APPCENTER_SOURCE_DIRECTORY/app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk  \
     --test-series "espresso-tests" \
     --locale "en_US" \
-    --build-dir $APPCENTER_OUTPUT_DIRECTORY/app-debug.apk \
+    --build-dir $APPCENTER_OUTPUT_DIRECTORY \
     --token $appCenterLoginApiToken
 
-
+#--app-path $APPCENTER_OUTPUT_DIRECTORY/app-debug-androidTest.apk  \
 
 #appcenter test run uitest \
 #    --app "tomso/Pickster" \
