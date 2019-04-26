4260c404784198d6955ee1461888616c0d12d7eb
Gary Cheng <d9369604@gmail.com>
2019-04-26 11:42:21 +0800

    copy the androidTest apk

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -10,7 +10,8 @@ echo 'ls output folder'
 ls $APPCENTER_OUTPUT_DIRECTORY
 
 /Users/vsts/agent/2.150.0/work/1/s/gradlew assembleAndroidTest
-/Users/vsts/agent/2.150.0/work/1/s/gradlew copyAndroidTestApk
+
+cp $APPCENTER_SOURCE_DIRECTORY/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk $APPCENTER_OUTPUT_DIRECTORY
 
 appcenter test run espresso \
     --app "d9369604-gmail.com/Hello-Music" \
