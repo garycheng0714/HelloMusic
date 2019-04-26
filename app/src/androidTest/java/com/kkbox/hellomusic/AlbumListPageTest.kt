7bf8875143d5491ef075506d0b930263e62b8780
Gary Cheng <d9369604@gmail.com>
2019-04-26 14:14:36 +0800

    Add Test-Cloud Espresso extension

--- app/src/androidTest/java/com/kkbox/hellomusic/AlbumListPageTest.kt
+++ app/src/androidTest/java/com/kkbox/hellomusic/AlbumListPageTest.kt
@@ -5,11 +5,24 @@ import com.kkbox.hellomusic.page.AlbumList
 import com.kkbox.hellomusic.page.Home
 import org.junit.Rule
 import org.junit.Test
+import com.microsoft.appcenter.espresso.Factory
+import com.microsoft.appcenter.espresso.ReportHelper
+import org.junit.After
+
+
 
 class AlbumListPageTest {
 
     @get:Rule
     val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Rule
+    var reportHelper: ReportHelper = Factory.getReportHelper()
+
+    @After
+    fun tearDown() {
+        reportHelper.label("Stopping App")
+    }
 
     @Test
     fun openAlbumList() {
